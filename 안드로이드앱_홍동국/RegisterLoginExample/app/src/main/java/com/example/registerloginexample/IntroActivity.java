df7d7d599d4ae2f48d2c8b61a64b14f9b423aee4
ghdehdrnr31 <ghdehdrnr31@naver.com>
2020-05-14 16:51:20 +0900

    05_14

---
+++ 안드로이드앱_홍동국/RegisterLoginExample/app/src/main/java/com/example/registerloginexample/IntroActivity.java
@@ -0,0 +1,29 @@
+package com.example.registerloginexample;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class IntroActivity extends Activity {
+    @Override
+    protected  void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.intro_layout);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },1500);
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        finish();
+    }
+}
