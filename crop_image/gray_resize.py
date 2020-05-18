065122225b44222c5280e155ad4fda068ff2acba
choseungje <wean1812@naver.com>
2020-05-18 21:40:57 +0900

    pretreatment cat, dino, fox

---
+++ crop_image/gray_resize.py
@@ -0,0 +1,13 @@
+import cv2
+import os
+
+for num in range(1000):
+    # filePath 는 폴더이름/1.jpg 형식
+    filePath = "fox/fox_" + str(num + 1) + '.jpg'
+    print(filePath)
+
+    path = cv2.imread(filePath, cv2.IMREAD_GRAYSCALE)
+    print(path)
+    resize = cv2.resize(path, dsize=(128, 128), interpolation=cv2.INTER_AREA)
+    cv2.imwrite("../picture/gray_resize_picture/fox_" + str(num+1) + ".jpg", resize)
+    print("%d번 사진 resize" % (num+1))
